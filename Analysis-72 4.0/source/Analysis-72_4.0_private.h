b4654e97ae1c8220e17116d780fe15d1f3a5feba
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2023-04-10 14:02:28 -0400

    Added Analysis-72 4.0
    The first release since 2018, Analysis-72 4.0 is a basic recoding of Analysis-72 3.1 which contains under-the-hood improvements, a new (original) icon, and proper use of multiple file extensions, with the Analysis-72 2.0 file format (*.data_analyze) being removed due to compatibility with older systems. Plus, Analysis-72 is now 32-bit, allowing for better compatibility.

---
+++ Analysis-72 4.0/source/Analysis-72_4.0_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef ANALYSIS-72_4_0_PRIVATE_H
+#define ANALYSIS-72_4_0_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"4.0.2.26"
+#define VER_MAJOR	4
+#define VER_MINOR	0
+#define VER_RELEASE	2
+#define VER_BUILD	26
+#define COMPANY_NAME	"SteelsOfLiquid - https://github.com/steelsofliquid"
+#define FILE_VERSION	"4.0.2.26"
+#define FILE_DESCRIPTION	"Analysis-72 4.0"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	""
+#define PRODUCT_VERSION	"4.0.2.26"
+
+#endif /*ANALYSIS-72_4_0_PRIVATE_H*/
